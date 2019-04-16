15a522f432d2bad28c6e03ffbdbf7b7db702ed13
hahnec <inbox@christopherhahne.de>
2019-04-17 00:02:20 +0200

    style(comment): remove redundant comment

--- plenoptisign/refo.py
+++ plenoptisign/refo.py
@@ -23,7 +23,6 @@ Copyright (c) 2019 Christopher Hahne <inbox@christopherhahne.de>
 import numpy as np
 from .solver import solve_sle
 from plenoptisign.constants import PlenoptisignError, DEC_P
-#from . import constants as c
 
 
 class Mixin:
